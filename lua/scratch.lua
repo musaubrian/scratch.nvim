802672089d4f1e581a95617e0881484e81ef7ad7
musaubrian <musaubrian15@gmail.com>
2023-06-26 10:13:58 +0300

    feat: add command to detach and re-attach to the _scratch buffer

--- lua/scratch.lua
+++ lua/scratch.lua
@@ -3,13 +3,6 @@ local M = {}
 local group = vim.api.nvim_create_augroup("scratch", {})
 local enabled = false
 
-local function switchToBufferIfExists(bufName)
-    local bufNum = vim.fn.bufnr(bufName)
-    if bufNum ~= -1 then
-        vim.cmd('buffer ' .. bufNum)
-    end
-end
-
 M.enabled = function()
     enabled = true
     if enabled then
--- plugin/scratch.lua
+++ plugin/scratch.lua
@@ -1,8 +1,9 @@
 vim.api.nvim_create_user_command("Scratch", function()
     require("scratch").toggle(true)
 end, {})
-
 vim.api.nvim_create_user_command("Cscratch", function()
     require("scratch").toggle(false)
 end, {})
-
+vim.api.nvim_create_user_command("Tscratch", function()
+    require("scratch").toggle()
+end, {})
