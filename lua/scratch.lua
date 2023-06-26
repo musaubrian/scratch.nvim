df2d4ca99d5401c227cc50fdaf55ade5f2245d75
musaubrian <musaubrian15@gmail.com>
2023-06-26 09:56:31 +0300

    feat: attach to buffer if it exists

--- lua/scratch.lua
+++ lua/scratch.lua
@@ -3,23 +3,32 @@ local M = {}
 local group = vim.api.nvim_create_augroup("scratch", {})
 local enabled = false
 
+local function switchToBufferIfExists(bufName)
+    local bufNum = vim.fn.bufnr(bufName)
+    if bufNum ~= -1 then
+        vim.cmd('buffer ' .. bufNum)
+    end
+end
+
 M.enabled = function()
     enabled = true
     if enabled then
-        vim.cmd.vsplit()
-        -- vim.cmd('split')
+        switchToBufferIfExists("_scratch")
+        vim.cmd('vsplit splitright')
         vim.cmd('noswapfile hide enew')
         vim.cmd('setlocal buftype=nofile')
         vim.cmd('setlocal bufhidden=hide')
         -- vim.cmd('setlocal nobuflisted')
-        vim.cmd('file scratch')
+        vim.cmd('file _scratch')
     end
 end
 
 M.disable = function()
     enabled = false
+    vim.cmd('bdelete _scratch')
     vim.api.nvim_clear_autocmds { group = group }
 end
+
 
 M.toggle = function(val)
     if val == nil then
